74837c27a18d61680413d010137ad29e05185442
muxammadziz <muxammadazizramziddinov@gmail.com>
2022-06-18 12:14:26 +0500

    update middleware

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ app.use(express.json())
 app.use(cors());
 app.use(fileUpload())
 app.use(validate)
-app.use(checkToken)
 app.use(express.static(path.join(__dirname, 'uploads')))
+app.use(checkToken)
 
 
 
